e7cb0052e5aed4286e01a4b2f64e9a5d7d20ad1e
Akash Kumar <Akash.dhaliya@gmail.com>
2020-07-13 23:59:36 +0530

    Facility Screen

--- src/Components/FacilityTimeSlotComponent/FacilityTimeSlot.jsx
+++ src/Components/FacilityTimeSlotComponent/FacilityTimeSlot.jsx
@@ -9,9 +9,11 @@ function TimeSlot(props) {
         day: item.day,
         checked: false,
         timeFrom: "",
-        fromAMPM: "",
+        timeFromAM: false,
+        timeFromPM: false,
         timeTo: "",
-        toAMPM: "",
+        timeToAM: false,
+        timeToPM: false,
         timeFromerror: false,
         timeToerror: false,
       };
@@ -36,11 +38,56 @@ function TimeSlot(props) {
     }
   }
 
+  function checkedhandler(evt) {
+    let day = evt.target.name;
+    let data = facilityData.map((item) => {
+      if (item.day === day) {
+        item.checked = evt.target.checked;
+      }
+      return item;
+    });
+    setFacilityData(data);
+  }
+
+  function applyToAllHandler(evt) {
+    let day = evt.target.name;
+    let selectedData = facilityData.filter(
+      (item) =>
+        item.day === day &&
+        item.timeFrom.length !== 0 &&
+        item.timeTo.length !== 0
+    );
+    if (selectedData.length !== 0) {
+      let data = facilityData.map((item) => {
+        if (item.checked) {
+          let data = {
+            day: item.day,
+            checked: true,
+            timeFrom: selectedData[0].timeFrom,
+            timeFromAM: selectedData[0].timeFromAM,
+            timeFromPM: selectedData[0].timeFromPM,
+            timeTo: selectedData[0].timeTo,
+            timeToAM: selectedData[0].timeToAM,
+            timeToPM: selectedData[0].timeToPM,
+            timeFromerror: selectedData[0].timeFromerror,
+            timeToerror: selectedData[0].timeToerror,
+          };
+          return data;
+        }
+        return item;
+      });
+      setFacilityData(data);
+    }
+    evt.preventDefault();
+  }
+
   function setTime(evt) {
     let time = evt.target.value;
     let timeField = evt.target.className.split(" ")[0];
     let day = evt.target.getAttribute("day");
     let error = false;
+    let AM = false;
+    let PM = false;
     if (time.length === 2 && evt.nativeEvent.inputType.includes("insertText")) {
       time = time + ":";
     }
@@ -51,12 +98,18 @@ function TimeSlot(props) {
       if (!error) {
         let splittedHHMM = time.split(":");
         if (parseInt(splittedHHMM[0]) >= 12) {
+          AM = false;
+          PM = true;
+
           let calculateHH = parseInt(splittedHHMM[0] - 12);
           splittedHHMM[0] =
             calculateHH < 10
               ? "0" + calculateHH.toString()
               : calculateHH.toString();
           time = splittedHHMM.join(":");
+        } else {
+          AM = true;
+          PM = false;
         }
       }
     }
@@ -64,6 +117,8 @@ function TimeSlot(props) {
       if (item.day === day) {
         item[timeField] = time;
         item[timeField + "error"] = error;
+        item[timeField + "AM"] = AM;
+        item[timeField + "PM"] = PM;
       }
       return item;
     });
@@ -88,14 +143,19 @@ function TimeSlot(props) {
           </div>
           {facilityData.map((item) => (
             <div key={item.day} className="facilityContentDiv">
-              <input type="checkbox" name={item.day} value={item.day}></input>
+              <input
+                type="checkbox"
+                name={item.day}
+                checked={item.checked}
+                onChange={checkedhandler}
+                value={item.day}
+              ></input>
               <label className={"TimeSlot-label"} htmlFor={item.day}>
                 {item.day}
               </label>
               <span>
                 <input
                   maxLength="5"
-                  id=""
                   className={
                     item.timeFromerror
                       ? "timeFrom facilityInputFromError"
@@ -108,8 +168,20 @@ function TimeSlot(props) {
                   value={item.timeFrom}
                 ></input>
                 <span>
-                  <span className="AMSpan">AM</span>
-                  <span className="PMSpan">PM</span>
+                  <span
+                    className={
+                      item.timeFromAM ? "AMSpan activeAMPMSpan" : "AMSpan"
+                    }
+                  >
+                    AM
+                  </span>
+                  <span
+                    className={
+                      item.timeFromPM ? "PMSpan activeAMPMSpan" : "PMSpan"
+                    }
+                  >
+                    PM
+                  </span>
                 </span>
               </span>
               <span>
@@ -125,12 +197,33 @@ function TimeSlot(props) {
                   value={item.timeTo}
                 ></input>
                 <span>
-                  <span className="AMSpan">AM</span>
-                  <span className="PMSpan">PM</span>
+                  <span
+                    className={
+                      item.timeToAM ? "AMSpan activeAMPMSpan" : "AMSpan"
+                    }
+                  >
+                    AM
+                  </span>
+                  <span
+                    className={
+                      item.timeToPM ? "PMSpan activeAMPMSpan" : "PMSpan"
+                    }
+                  >
+                    PM
+                  </span>
                 </span>
               </span>
               <span>
-                <button className="applyToAllBtn">Apply to All Checked</button>
+                <button
+                  disabled={isError}
+                  className={
+                    isError ? "btnDisabled applyToAllBtn" : "applyToAllBtn"
+                  }
+                  name={item.day}
+                  onClick={applyToAllHandler}
+                >
+                  Apply to All Checked
+                </button>
               </span>
             </div>
           ))}
@@ -140,7 +233,7 @@ function TimeSlot(props) {
               Cancel
             </button>
             <button
-              className={isError ? "submitBtnDisabled" : ""}
+              className={isError ? "btnDisabled" : ""}
               type="submit"
               disabled={isError}
             >
